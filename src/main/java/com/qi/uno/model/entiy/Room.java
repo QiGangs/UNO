27064a5c61a60800ee4a71b6773d33f2583b59cf
qigang <a646608986@gmail.com>
2018-07-06 13:31:31 +0800

    修正房间模型

--- src/main/java/com/qi/uno/model/entiy/Room.java
+++ src/main/java/com/qi/uno/model/entiy/Room.java
@@ -20,16 +20,20 @@ public class Room {
     private int direction;
     private LinkedBlockingQueue<Card> cardPile;
     private ArrayList<Card> discardPile;
+    private Long mainPlayerId;
 
-    public Room(Long roomId, int playerNum) {
+    public Room(Long roomId, int playerNum,Player mainPlayer) {
         this.roomId = roomId;
         this.playerNum = playerNum;
         this.players = Lists.newArrayListWithCapacity(playerNum);
         this.direction = RoomStatus.DISCARD_DIRECTION_NEXT;
         this.cardPile = new LinkedBlockingQueue<>(CardStatus.CARD_ALL_NUM);
         this.discardPile = Lists.newArrayListWithCapacity(CardStatus.CARD_ALL_NUM);
+        this.mainPlayerId = mainPlayer.getPlayerId();
+        addPlayer(mainPlayer);
     }
 
+    //房间行为逻辑
     public void addPlayer(Player player){
 
     }
@@ -44,5 +48,77 @@ public class Room {
 
     public void gameOver(){
 
+    }
+
+
+
+    //get set
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getPlayerNum() {
+        return playerNum;
+    }
+
+    public void setPlayerNum(int playerNum) {
+        this.playerNum = playerNum;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public LinkedBlockingQueue<Card> getCardPile() {
+        return cardPile;
+    }
+
+    public void setCardPile(LinkedBlockingQueue<Card> cardPile) {
+        this.cardPile = cardPile;
+    }
+
+    public ArrayList<Card> getDiscardPile() {
+        return discardPile;
+    }
+
+    public void setDiscardPile(ArrayList<Card> discardPile) {
+        this.discardPile = discardPile;
+    }
+
+    public Long getMainPlayerId() {
+        return mainPlayerId;
+    }
+
+    public void setMainPlayerId(Long mainPlayerId) {
+        this.mainPlayerId = mainPlayerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomId=" + roomId +
+                ", playerNum=" + playerNum +
+                ", players=" + players +
+                ", direction=" + direction +
+                ", cardPile=" + cardPile +
+                ", discardPile=" + discardPile +
+                ", mainPlayerId=" + mainPlayerId +
+                '}';
     }
 }
