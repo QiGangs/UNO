1bc4fb9ae50e5b61ddb006d61d4fa9b9a1c34140
qigang <a646608986@gmail.com>
2018-07-06 13:39:23 +0800

    添加判牌服务

---
+++ src/main/java/com/qi/uno/service/JudgeService.java
@@ -0,0 +1,9 @@
+package com.qi.uno.service;
+
+/**
+ * @description:
+ * @author: qigang
+ * @create: 2018-07-06 13:38
+ **/
+public interface JudgeService {
+}
---
+++ src/main/java/com/qi/uno/service/impl/JudgeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.qi.uno.service.impl;
+
+import com.qi.uno.service.JudgeService;
+
+/**
+ * @description: 描述判牌服务
+ * @author: qigang
+ * @create: 2018-07-06 13:38
+ **/
+public class JudgeServiceImpl implements JudgeService {
+}
