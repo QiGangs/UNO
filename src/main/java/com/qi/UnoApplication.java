2d9effbb14ec788f129cb35648007e9682bc553b
qigang <a646608986@gmail.com>
2018-12-05 21:05:20 +0800

    游戏版本1.0.03 完善了ssl 部署上线

--- src/main/java/com/qi/UnoApplication.java
+++ src/main/java/com/qi/UnoApplication.java
@@ -12,4 +12,38 @@ public class UnoApplication {
         springApplication.addListeners(new ApplicationStartup());
         springApplication.run(args);
     }
+
+//    /**
+//     * http重定向到https
+//     * @return
+//     */
+//    @Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        //Connector监听的http的端口号
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        //监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
+
 }
